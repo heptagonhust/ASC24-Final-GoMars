2fb5326c294714b20c749dbada7142a694b3a2d6
Li Dong <dongli@lasg.iap.ac.cn>
2023-04-23 09:05:12 +0800

    Change ccpp-physics url and test GFS_v16 suite

--- src/physics/ccpp/clean.sh
+++ src/physics/ccpp/clean.sh
@@ -3,3 +3,5 @@
 ./framework/scripts/ccpp_prebuild.py \
   --config host/ccpp_prebuild_config.py \
   --clean
+
+rm host/CCPP_STATIC_API.cmake
--- src/physics/ccpp/run.sh
+++ src/physics/ccpp/run.sh
@@ -2,4 +2,4 @@
 
 ./framework/scripts/ccpp_prebuild.py \
   --config host/ccpp_prebuild_config.py \
-  --suites FV3_GFS_v17_p8_rrtmgp
+  --suites FV3_GFS_v16
