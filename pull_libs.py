a429d8e9b3975cbe95f90af26bcd277d6ae612f5
Li Dong <dongli@lasg.iap.ac.cn>
2024-01-01 10:27:32 +0800

    Add lib rrtmgp

--- pull_libs.py
+++ pull_libs.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 
 parser = argparse.ArgumentParser(description='Pulls all or some submodules from external repositories.')
-parser.add_argument('--libs', nargs='+', help='The libraries to pull.', choices=('noahmp',), default=[])
+parser.add_argument('--libs', nargs='+', help='The libraries to pull.', choices=('noahmp','rrtmgp'), default=[])
 args = parser.parse_args()
 
 def run(cmd):
@@ -25,7 +25,16 @@ pull('lib/fiona')
 pull('lib/flogger')
 pull('lib/string')
 
+project_root = os.getcwd()
+
 if 'noahmp' in args.libs:
 	pull('lib/noahmp')
 	os.chdir('lib/noahmp')
 	run('git apply ../noahmp.diff')
+	os.chdir(project_root)
+
+if 'rrtmgp' in args.libs:
+	pull('lib/rrtmgp')
+	os.chdir('lib/rrtmgp')
+	run('git apply ../rrtmgp.diff')
+	os.chdir(project_root)
---
+++ src/physics/wrf/rad_rrtmgp_mod.F90
@@ -0,0 +1,16 @@
+module rad_rrtmgp_mod
+
+  use mo_rte_kind
+  use mo_optical_props
+  use mo_gas_optics_rrtmgp
+  use mo_aerosol_optics_rrtmgp_merra
+  use mo_gas_concentrations
+  use mo_source_functions
+  use mo_fluxes
+  use mo_rte_lw
+  use mo_rte_sw
+  use mo_rte_config
+
+  implicit none
+
+end module rad_rrtmgp_mod
