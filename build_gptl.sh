5af4587637472318d0e2b4d96b2d5311f6fc2224
SSK015 <18252677381@163.com>
2024-02-22 19:52:31 +0800

    feat: add ncl and dynamic gptl to main

---
+++ build_gptl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+spack load cmake@3.24.4
+spack load intel-oneapi-compilers@2024.0.1
+spack load intel-oneapi-mkl@2024.0.0
+spack load intel-oneapi-mpi@2021.11.0
+# spack load libxml2
+# spack load curl
+# spack load hdf5/fxhrrhv
+
+export CC=mpiicx
+export FC=mpiifort
+export F77=mpiifort
+
+current_dir=$(pwd)
+target_dir="$current_dir/gmcore/gptl"
+
+if [ ! -d "$target_dir" ]; then
+    mkdir -p "$target_dir"
+fi
+
+pushd gmcore
+pushd gptl
+CDIR=$(pwd)
+wget https://github.com/jmrosinski/GPTL/releases/download/v8.1.1/gptl-8.1.1.tar.gz
+tar -zxvf gptl-8.1.1.tar.gz
+pushd gptl-8.1.1
+
+# wget https://gist.githubusercontent.com/bonfus/21dec6b966859f5f509b935f8b055a7f/raw/macros.make
+
+./configure --enable-pmpi --prefix=${CDIR}
+
+# make check
+
+make install
+
+popd
+popd
+popd
---
+++ install_conda.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ~/miniconda3
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
