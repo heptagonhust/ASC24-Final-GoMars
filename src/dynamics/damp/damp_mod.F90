770c061ad6c71cb52664eae0c98969319374daf6
Li Dong <dongli@lasg.iap.ac.cn>
2024-03-26 09:34:28 +0800

    Revise polar u and v nudging

--- src/dynamics/damp/damp_mod.F90
+++ src/dynamics/damp/damp_mod.F90
@@ -53,15 +53,22 @@ contains
 
     integer j
 
+    integer i, k
+    real(r8) c
+    associate (mesh => block%mesh, u_lon => dstate%u_lon, v_lat => dstate%v_lat)
     ! This nudging of polar v helps to keep the flow neat around the poles.
     ! NOTE: DO NOT REMOVE IT!
-    do j = block%mesh%half_jms, block%mesh%half_jme
-      if (block%mesh%is_south_pole(j)) then
-        dstate%v_lat%d(:,j,:) = 0.8_r8 * dstate%v_lat%d(:,j,:) + 0.2_r8 * dstate%v_lat%d(:,j+1,:)
-      else if (block%mesh%is_north_pole(j+1)) then
-        dstate%v_lat%d(:,j,:) = 0.8_r8 * dstate%v_lat%d(:,j,:) + 0.2_r8 * dstate%v_lat%d(:,j-1,:)
+    c = 0.8_r8
+    do j = mesh%half_jms, mesh%half_jme
+      if (mesh%is_south_pole(j)) then
+        u_lon%d(:,j+1,:) = c * u_lon%d(:,j+1,:) + (1 - c) * u_lon%d(:,j+2,:)
+        v_lat%d(:,j  ,:) = c * v_lat%d(:,j  ,:) + (1 - c) * v_lat%d(:,j+1,:)
+      else if (mesh%is_north_pole(j+1)) then
+        u_lon%d(:,j  ,:) = c * u_lon%d(:,j  ,:) + (1 - c) * u_lon%d(:,j-1,:)
+        v_lat%d(:,j  ,:) = c * v_lat%d(:,j  ,:) + (1 - c) * v_lat%d(:,j-1,:)
       end if
     end do
+    end associate
 
     if (use_vor_damp) then
       do j = 1, vor_damp_cycles
